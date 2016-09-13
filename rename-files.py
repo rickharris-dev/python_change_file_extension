e4000f777b456f2bc356acc883cc85e0ff935367
rickharris-dev <rickharris724@gmail.com>
2016-09-13 11:05:22 -0700

    Uses regex to match filenames in path to update file extension

---
+++ rename-files.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+from sys import argv
+from os import listdir, rename
+from re import compile, match
+
+def rename_extension(path, extension, new_extension):
+	expression = compile('.*\.' + extension + '$')
+	files = [file for file in listdir(path) if expression.match(file)]
+
+	for file in files:
+		try:
+			old_name = path + '/' + file
+			new_name = path + '/' + file[:-3:] + new_extension
+			rename(old_name, new_name)
+			print 'Updated ' + new_name
+		except:
+			print 'Error: Could not rename ' + old_name
+
+if __name__ == '__main__':
+	path = argv[1]
+	extension = argv[2]
+	new_extension = argv[3]
+	rename_extension(path, extension, new_extension)
