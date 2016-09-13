856a0ea1c961aff2b116e2e36bae7f2ff7304b50
rickharris-dev <rickharris724@gmail.com>
2016-09-13 11:24:06 -0700

    Checks for / on path and provides output if no files

--- rename-files.py
+++ rename-files.py
@@ -7,14 +7,22 @@ def rename_extension(path, extension, new_extension):
 	expression = compile('.*\.' + extension + '$')
 	files = [file for file in listdir(path) if expression.match(file)]
 
-	for file in files:
-		try:
-			old_name = path + '/' + file
-			new_name = path + '/' + file[:-3:] + new_extension
-			rename(old_name, new_name)
-			print 'Updated ' + new_name
-		except:
-			print 'Error: Could not rename ' + old_name
+	# Check if path has / at the end
+	if not compile('.*/$').match(path):
+		path = path + '/'
+		
+	# Update files
+	if len(files) == 0:
+		print 'No files to update'
+	else:
+		for file in files:
+			try:
+				old_name = path + file
+				new_name = path + file[:-3:] + new_extension
+				rename(old_name, new_name)
+				print 'Updated ' + new_name
+			except:
+				print 'Error: Could not rename ' + old_name
 
 if __name__ == '__main__':
 	path = argv[1]
