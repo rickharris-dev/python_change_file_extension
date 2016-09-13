dfd349529a2b3ee7001c61af481e7805c5f6c0f0
rickharris-dev <rickharris724@gmail.com>
2016-09-13 14:35:36 -0700

    Added path and extension checking

--- rename-files.py
+++ rename-files.py
@@ -1,31 +1,53 @@
 #!/usr/bin/python
 from sys import argv
-from os import listdir, rename
+from os import listdir, path, rename
 from re import compile, match
 
-def rename_extension(path, extension, new_extension):
-	expression = compile('.*\.' + extension + '$')
-	files = [file for file in listdir(path) if expression.match(file)]
+def rename_extension(file_path, extension, new_extension):
+	try:
+		# Check if path exists
+		path.exists(file_path)
 
-	# Check if path has / at the end
-	if not compile('.*/$').match(path):
-		path = path + '/'
-		
-	# Update files
-	if len(files) == 0:
-		print 'No files to update'
-	else:
-		for file in files:
-			try:
-				old_name = path + file
-				new_name = path + file[:-3:] + new_extension
-				rename(old_name, new_name)
-				print 'Updated ' + new_name
-			except:
-				print 'Error: Could not rename ' + old_name
+		# Check if is directory
+		path.isdir(file_path)
+
+		# Check if path has / at the end
+		if not compile('.*/$').match(file_path):
+			file_path = file_path + '/'
+
+		# Check if extensions have . at the front
+		extension_test = compile('^\..*')
+		if extension_test.match(extension):
+			extension = extension[1::]
+
+		if extension_test.match(new_extension):
+			new_extension = new_extension[1::]
+
+		# Create regex and find matching files
+		expression = compile('.*\.' + extension + '$')
+		files = [file for file in listdir(file_path) if expression.match(file)]
+
+		# Update file extensions
+		if len(files) == 0:
+			print 'No files to update'
+		else:
+			for file in files:
+				try:
+					# Rename each file
+					old_name = file_path + file
+					new_name = file_path + file[:-3:] + new_extension
+					rename(old_name, new_name)
+					print 'Updated ' + new_name
+				except:
+					print 'Error: Could not rename ' + old_name
+	except Exception as e:
+		if 'Not a directory' in e:
+			print "The path given is not a directory"
+		else:
+			print "File path does not exist"
 
 if __name__ == '__main__':
-	path = argv[1]
+	file_path = argv[1]
 	extension = argv[2]
 	new_extension = argv[3]
-	rename_extension(path, extension, new_extension)
+	rename_extension(file_path, extension, new_extension)
